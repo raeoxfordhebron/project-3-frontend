7249eb6a09a67a4cfcf263f453aec87e83e94642
Ruhama Yared <ruhamayared1@gmail.com>
2023-01-22 15:39:58 -0500

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Outlet />
+      <Footer />
     </div>
   )
 }
