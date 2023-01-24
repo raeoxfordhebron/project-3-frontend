80aac73b24e919e24e2f6940a74c11ae173e7934
Brandon Haupt <haupt.brandon@gmail.com>
2023-01-24 15:55:38 -0600

    updated app.js
    - added missing components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
+import { Outlet } from "react-router-dom"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 function App() {
   return (
     <div className="App">
-
+      <Header />
+      <Outlet />
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
