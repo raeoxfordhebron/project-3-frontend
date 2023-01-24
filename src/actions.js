904286fc487d3d911cbd378e98048fb89cd84e62
samwhitford11 <samantha.fichte98@gmail.com>
2023-01-23 19:54:38 -0700

    adds create, update, delete actions

---
+++ src/actions.js
@@ -0,0 +1,57 @@
+import { redirect } from "react-router-dom"
+
+// const URL = " "
+
+// CREATE ACTION
+export const createAction = async ({request}) => {
+    const formData = await request.formData();
+
+    const newPlace = {
+        place: formData.get("place"),
+        cityState: formData.get("cityState"),
+        address: formData.get("address"),
+        url: formData.get("url"),
+        image: formData.get("image"),
+        notes: formData.get("notes"),
+    };
+
+    await fetch(URL + '/places', {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newPlace),
+    });
+    return redirect('/');
+};
+
+// UPDATE ACTION
+export const updateAction = async ({ request, params }) => {
+    const formData = await request.formData()
+  
+    const updatedPlace = {
+        place: formData.get("place"),
+        cityState: formData.get("cityState"),
+        address: formData.get("address"),
+        url: formData.get("url"),
+        image: formData.get("image"),
+        notes: formData.get("notes"),
+    }
+  
+    await fetch(URL + "/places/" + params.id, {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedPlace),
+    })
+    return redirect("/")
+  };
+  
+  // DELETE ACTION
+  export const deleteAction = async ({ params }) => {
+    await fetch(URL + "/places/" + params.id, {
+      method: "delete",
+    })
+    return redirect("/")
+  }
