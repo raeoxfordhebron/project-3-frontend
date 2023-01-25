b601431a0dbd144aa33c1ffa296a095cdc2b325e
samwhitford11 <samantha.fichte98@gmail.com>
2023-01-24 18:05:27 -0700

    imports actions to router

--- src/router.js
+++ src/router.js
@@ -3,15 +3,16 @@ import App from "./App"
 import Index from "./pages/Index"
 import Show from "./pages/Show"
 import { indexLoader, showLoader } from "./loaders"
+import { createAction, updateAction, deleteAction } from "./actions"
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route path="" element={<Index />} loader={indexLoader}/>
       <Route path=":id" element={<Show />} loader={showLoader}/>
-      <Route path="create" />
-      <Route path="update/:id" />
-      <Route path="delete/:id" />
+      <Route path="create" action={createAction}/>
+      <Route path="update/:id" action={updateAction}/>
+      <Route path="delete/:id" action={deleteAction}/>
     </Route>
   )
 )
