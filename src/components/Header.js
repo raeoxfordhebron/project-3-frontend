64995c1eb6afb758795a181aae05602b3d73021a
samwhitford11 <samantha.fichte98@gmail.com>
2023-01-25 20:09:23 -0700

    adds icon to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
-
+// import { FaCaravan } from "react-icons/fa";
+import icon from "./img/car.png"
 
 export default function Header(){
     
     return(
         <div className="header">
-            <p className="cName">Road Trip Buddy</p>
+            <p className="cName">Road Trip Buddy <img className="trailer" src={icon} alt="car icon"/> </p>
             <div className="nav">
                 <p>box1</p>
                 <p>box2</p>
@@ -12,3 +13,6 @@ export default function Header(){
         </div>
     )
 }
+
+
+
