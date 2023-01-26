f994049d5d1a1c2b054ff7e76e1fa67d0a22d64d
Ruhama Yared <ruhamayared1@gmail.com>
2023-01-26 15:16:50 -0500

    checking out styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,20 @@
+import { useState } from "react"
 
+export default function Header() {
+  const [navbarOpen, setNavbarOpen] = useState(false)
 
-export default function Header(){
+  const handleToggle = () => {
+    setNavbarOpen(!navbarOpen)
+  }
 
-    const [navbarOpen, setNavbarOpen] = useState(false)
-
-    const handleToggle = () => {
-        setNavbarOpen(!navbarOpen)
-    }
-
-    return(
-        <div className="header">
-            <p className="cName">Road Trip Buddy</p>
-            <div className="nav">
-                <p>box1</p>
-                <button onClick={handleToggle}>{navbarOpen ? "X" : "="}</button>
-                <ul className={`menuNav ${navbarOpen ? " showMenu" : ""}`}>
-                    
-                </ul>
-                </div>
-        </div>
-    )
+  return (
+    <div className="header">
+      <p className="cName">Road Trip Buddy</p>
+      <div className="nav">
+        <p>box1</p>
+        <button onClick={handleToggle}>{navbarOpen ? "X" : "="}</button>
+        <ul className={`menuNav ${navbarOpen ? " showMenu" : ""}`}></ul>
+      </div>
+    </div>
+  )
 }
-
-
-
