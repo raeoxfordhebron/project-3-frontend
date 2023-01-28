1322e74552aed77a0b4c05b489448af37ba2e6c0
Rae Hebron <113301611+raeoxfordhebron@users.noreply.github.com>
2023-01-27 20:54:40 -0500

    Revert "User auth"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import { Outlet } from "react-router-dom"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
-import React from "react"
-import {useState} from "react"
-
-export const GlobalCtx = React.createContext(null)
-
 
 function App() {
-  const [gState, setGState] = React.useState({url: "https://project-3-backend-cm7x.onrender.com"})
-
   return (
-    <GlobalCtx.Provider value={{gState, setGState}}>
     <div className="App">
       <Header />
       <Outlet />
       <Footer />
     </div>
-    </GlobalCtx.Provider>
   )
 }
 
--- src/router.js
+++ src/router.js
@@ -2,18 +2,17 @@ import { createBrowserRouter, createRoutesFromElements, Route } from "react-rout
 import App from "./App"
 import Index from "./pages/Index"
 import Show from "./pages/Show"
-import Signup from "./pages/Signup"
 import { indexLoader, showLoader } from "./loaders"
+import { createAction, updateAction, deleteAction } from "./actions"
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
-      <Route path="" element={<Index />} loader={indexLoader}/>
-      <Route path=":id" element={<Show />} loader={showLoader}/>
-      <Route path="create" />
-      <Route path="update/:id" />
-      <Route path="delete/:id" />
-      <Route path="signup" element={<Signup />}/>
+      <Route path="" element={<Index />} loader={indexLoader} />
+      <Route path=":id" element={<Show />} loader={showLoader} />
+      <Route path="create" action={createAction} />
+      <Route path="update/:id" action={updateAction} />
+      <Route path="delete/:id" action={deleteAction} />
     </Route>
   )
 )
